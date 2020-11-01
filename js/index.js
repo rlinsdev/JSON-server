89d2a902ebca8c5ad6076970e4b4653dceb45b80
Rodrigo Lins e Freitas <rlinsdev@gmail.com>
2020-11-01 12:04:25 -0300

    JSon Server

--- js/index.js
+++ js/index.js
@@ -1,21 +1,20 @@
 const container = document.querySelector('.blogs');
 
 const renderPosts = async () => {
-  let uri = 'http://localhost:3000/posts';
+  let uri = 'http://localhost:3000/posts?_sort=likes&_order=desc';
 
   const res = await fetch(uri);
 
   const posts = await res.json();
 
   let template = '';
-
   posts.forEach((post) => {
     template += `
         <div className="post">
             <h2>${post.title}</h2>
             <p><small>${post.likes}</small></p>
             <p>${post.body.slice(0, 200)}</p>
-            <a href="/details.html">read more...</a>
+            <a href="/details.html?id=${post.id}">read more...</a>
         </div>
         `;
   });
